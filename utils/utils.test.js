4956b3bd1ff92d3a331180e86fb9704243674d6c
Andy@UDN_mbp <f255431@gmail.com>
2018-02-09 16:19:18 +0800

    Using an Assertion Library (expect@1.20.2)

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,29 @@
+const expect = require('expect');
+
 const utils = require('./utils.js');
 
 it('should add two numbers', () => {
-    var result = utils.add(28, 13);
+    var result = utils.add(22, 13);
     
-    if (result !== 41){
-        throw new Error(`Expected 41, but got ${result}`);
-    }
+    expect(result).toBe(35, `Expected 35, but got ${result}`).toBeA('number');
 });
 
 it('should square a number', () => {
     var result = utils.square(4);
 
-    if (result !== 16){
-        throw new Error(`Expected 16, but got ${result}`);
-    }
+    expect(result).toBe(16, `Expected 16, but got ${result}`).toBeA('number');
+});
+
+it('should verify first and last name', () => {
+    var user = {
+        age: 27,
+        location: 'Taipei'
+    };
+
+    var result = utils.setName(user, 'Andy Fu');
+
+    expect(result).toInclude({
+        firstName: 'Andy',
+        lastName: 'Fu'
+    });
 });
