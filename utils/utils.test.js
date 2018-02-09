4fac0d0d8065eaca27027244740c67e21c47ae81
Andy@UDN_mbp <f255431@gmail.com>
2018-02-09 17:07:48 +0800

    Testing Asynchronous Code
    加入callback argument: done, 讓mocha 知道是在測試一個async function

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,10 +8,24 @@ it('should add two numbers', () => {
     expect(result).toBe(35, `Expected 35, but got ${result}`).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 6, (result) => {
+        expect(result).toBe(10).toBeA('number');
+        done();
+    });
+});
+
 it('should square a number', () => {
     var result = utils.square(4);
 
     expect(result).toBe(16, `Expected 16, but got ${result}`).toBeA('number');
+});
+
+it('should async square two numbers', (done) => {
+    utils.asyncSquare(5, (result) => {
+        expect(result).toBe(25).toBeA('number');
+        done();
+    });
 });
 
 it('should verify first and last name', () => {
