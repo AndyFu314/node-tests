d016d1aef122dd9c61db8030791786ac2a9540da
Andy@UDN_mbp <f255431@gmail.com>
2018-02-12 11:46:13 +0800

    Organizing Test With describe()
    使用mocha 的describe 來結構化測試案例

--- server/sever.test.js
+++ server/sever.test.js
@@ -3,29 +3,35 @@ const expect = require('expect');
 
 var app = require('./server.js').app;
 
-it('should return not found response', (done) => {
-    request(app)
-      .get('/')
-      .expect(404)
-      .expect('Content-Type', /json/)
-      .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'Page not found.'
-        })
-      })
-      .end(done);
-});
+describe('Server', () => {
+    describe('#GET /', () => {
+        it('should return not found response', (done) => {
+            request(app)
+              .get('/')
+              .expect(404)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'Page not found.'
+                })
+              })
+              .end(done);
+        });
+    });
 
-it('should return a user array that included me', (done) => {
-    request(app)
-      .get('/users')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect((res) => {
-          expect(res.body).toContain({
-              name: 'Andy',
-              age: 27
-          })
-      })
-      .end(done);
+    describe('#GET /users', () => {
+        it('should return a user array that included me', (done) => {
+            request(app)
+              .get('/users')
+              .expect(200)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                  expect(res.body).toContain({
+                      name: 'Andy',
+                      age: 27
+                  })
+              })
+              .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,30 +2,37 @@ const expect = require('expect');
 
 const utils = require('./utils.js');
 
-it('should add two numbers', () => {
-    var result = utils.add(22, 13);
+describe('Utils', () => {
+
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var result = utils.add(22, 13);
+            
+            expect(result).toBe(35, `Expected 35, but got ${result}`).toBeA('number');
+        });
+        
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 6, (result) => {
+                expect(result).toBe(10).toBeA('number');
+                done();
+            });
+        });
+    });
     
-    expect(result).toBe(35, `Expected 35, but got ${result}`).toBeA('number');
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 6, (result) => {
-        expect(result).toBe(10).toBeA('number');
-        done();
-    });
-});
-
-it('should square a number', () => {
-    var result = utils.square(4);
-
-    expect(result).toBe(16, `Expected 16, but got ${result}`).toBeA('number');
-});
-
-it('should async square two numbers', (done) => {
-    utils.asyncSquare(5, (result) => {
-        expect(result).toBe(25).toBeA('number');
-        done();
-    });
+    describe('#square', () => {
+        it('should square a number', () => {
+            var result = utils.square(4);
+        
+            expect(result).toBe(16, `Expected 16, but got ${result}`).toBeA('number');
+        });
+        
+        it('should async square two numbers', (done) => {
+            utils.asyncSquare(5, (result) => {
+                expect(result).toBe(25).toBeA('number');
+                done();
+            });
+        });
+    })
 });
 
 it('should verify first and last name', () => {
